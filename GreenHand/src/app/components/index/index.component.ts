68dc933a5fa823422970905607be46439fb3de49
Jeison Angel <jsangel@poligran.edu.co>
2021-11-27 11:09:40 -0500

    FUNCIONALIDAD: Filtro productos
    Se agrega validación de filtro en la lista de productos

--- GreenHand/src/app/components/index/index.component.ts
+++ GreenHand/src/app/components/index/index.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-index',
@@ -10,12 +11,28 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 export class IndexComponent implements OnInit {
 
   productSet = [];
+  productFil: Product[];
+  searchTerm: string;
   faCheckCircle = faCheckCircle;
+
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productSet = [];
     this.refreshList();
+  }
+
+  search(value: string): void {
+    for (let i = 0; i < this.productSet.length; i++) {
+      const ELEMENT = document.getElementById("card" + this.productSet[i].id);
+      const CONTENT = ELEMENT.innerText.toLowerCase();
+      if (CONTENT.indexOf(value.toLowerCase()) == -1) {
+        ELEMENT.classList.add("d-none");
+      } else {
+        ELEMENT.classList.remove("d-none");
+      }
+    }
   }
 
   retrieveProducts(): void {
